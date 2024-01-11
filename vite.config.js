9e34138686eb13c7e63923108dfcb26ee28e7149
Kleostro <maxim_zabaluev@icloud.com>
2024-01-11 07:22:07 +0300

    init: start tetris task

---
+++ vite.config.js
@@ -0,0 +1,40 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-extraneous-dependencies */
+import Inspect from 'vite-plugin-inspect';
+import { resolve } from 'path';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import handlebars from 'vite-plugin-handlebars';
+
+export default {
+  publicDir: 'assets',
+  plugins: [
+    Inspect(),
+    ViteImageOptimizer({
+      png: {
+        quality: 85,
+      },
+      jpg: {
+        quality: 85,
+      },
+      webp: {
+        quality: 70,
+      },
+    }),
+    handlebars({
+      partialDirectory: resolve(__dirname, './src/templates'),
+    }),
+  ],
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, './index.html'),
+        about: resolve(__dirname, './about/index.html'),
+        contacts: resolve(__dirname, './contacts/index.html'),
+      },
+    },
+    minify: false,
+    compact: false,
+    sourcemap: true,
+  },
+};
